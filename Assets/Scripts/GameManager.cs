2e21aac0ab1e14c726579864fb893671e0250048
ctm042 <ctm042@latech.edu>
2024-04-01 04:21:15 -0500

    Basic GameManager, created my scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public enum GameStateType {
+        Open,
+        Start,
+        SolvedPuzzle1,
+        SolvedPuzzle2,
+        SolvedPuzzle3,
+        SolvedPuzzle4,
+        SolvedPuzzle5,
+        End
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+public class GameManager : MonoBehaviour {
+    public static GameManager instance;
+    public GameStateType State { get; private set; }
+    
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+
+        State = GameStateType.Open;
+    }
+
+    public void ChangeState(GameStateType newState) {
+        State = newState;
     }
 }
