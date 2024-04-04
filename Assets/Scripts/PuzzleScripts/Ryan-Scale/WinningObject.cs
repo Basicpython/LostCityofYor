c0f63f00d101fda2eb6e9930dc2675dde40549ed
Ryan Saffel <rks023@email.latech.edu>
2024-04-04 17:44:42 -0500

    Scale Puzzle Demo Complete

---
+++ Assets/Scripts/PuzzleScripts/Ryan-Scale/WinningObject.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinningObject : MonoBehaviour
+{
+    public WeightScale weightScale;
+    public GameObject panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        weightScale = GameObject.Find("Left Side").GetComponent<WeightScale>();
+        panel.SetActive(false);
+    }
+
+    void objectSwitch(){
+        GameObject.Find("The Cube").GetComponent<Renderer>().material.color = new Color(0, 250, 0);
+        panel.SetActive(true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (weightScale.registeredRigidbodies == 3){
+            objectSwitch();
+        }
+    }
+}
