4ae1e5e9332cf0321b274bbe041610e0f8ced24a
Basicpython <39888296+Basicpython@users.noreply.github.com>
2024-04-04 19:23:15 -0500

    Post presentation adjustments

--- Assets/Scripts/PuzzleScripts/Ryan-Scale/WinningObject.cs
+++ Assets/Scripts/PuzzleScripts/Ryan-Scale/WinningObject.cs
@@ -16,6 +16,7 @@ public class WinningObject : MonoBehaviour
     void objectSwitch(){
         GameObject.Find("The Cube").GetComponent<Renderer>().material.color = new Color(0, 250, 0);
         panel.SetActive(true);
+		GameManager.instance.NextState();
     }
     // Update is called once per frame
     void Update()
