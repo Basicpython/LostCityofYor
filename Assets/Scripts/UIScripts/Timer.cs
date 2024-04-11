98b7602b4e0685f9a518e3081083ae6fa60207aa
ebm04 <139780145+ebm04@users.noreply.github.com>
2024-04-10 23:47:28 -0500

    Added Functional Timer
    Added Timer
    Im tired

---
+++ Assets/Scripts/UIScripts/Timer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float totalTime = 60.0f; 
+    private float currentTime; 
+    private TextMeshProUGUI textMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>(); 
+        currentTime = totalTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+          
+            int minutes = Mathf.FloorToInt(currentTime / 60);
+            int seconds = Mathf.FloorToInt(currentTime % 60);
+            textMesh.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+        else
+        {          
+            textMesh.text = "00:00";
+        }
+    }
+}
